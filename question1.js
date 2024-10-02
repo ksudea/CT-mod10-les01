ce67647a54ff3e18a5d2af60b0d49fc0a99a2e4d
ksudea <sudealmus@gmail.com>
2024-10-02 11:42:02 -0400

    Finished

---
+++ question1.js
@@ -0,0 +1,17 @@
+// Task 1
+let studentName;
+let studentAge;
+let studentGrade;
+let isPresent;
+
+// Task 2
+studentName = "Alice"; 
+studentAge = 15;   
+studentGrade = 10;  
+isPresent = true;  
+
+// Task 3
+console.log("Student Name: " + studentName);  
+console.log("Student Age: " + studentAge);  
+console.log("Student Grade: " + studentGrade); 
+console.log("Is Present: " + isPresent);    
---
+++ question2.js
@@ -0,0 +1,30 @@
+// Task 1
+let num1;
+let num2;
+// Task 2
+num1 = 5;
+num2 = 3;
+// Task 3
+let sum = num1 + num2;
+let diff = num1 - num2;
+let prod = num1 * num2;
+let quot = num1 / num2;
+console.log("Sum:", sum);
+console.log("Difference:", diff);
+console.log("Product:", prod);
+console.log("Quotient:", quot);
+// Task 4
+num1 *= 2;
+num2 += 4;
+console.log("Updated num1:", num1);
+console.log("Updated num2:", num2);
+// Task 5
+console.log("Is num1 equal to num2?", num1==num2);
+console.log("Is num1 greater than num2?", num1>num2);
+console.log("Is num1 not equal to num2?", num1 != num2);
+// Task 6
+let isPositive =  (num1 > 0) && (num2 > 0);
+let isEven = (num1 % 2 === 0) || (num2 % 2 === 0);
+console.log("Are both numbers positive?", isPositive);
+console.log("Is at least one number even?", isEven);
+
